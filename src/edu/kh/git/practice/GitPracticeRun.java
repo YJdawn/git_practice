184ed047eed438e45f61f378356dd1e5eabee33b
youngjin <qmffnqjem010@nate.com>
2023-02-20 19:20:13 +0900

    두번째 커밋

--- src/edu/kh/git/practice/GitPracticeRun.java
+++ src/edu/kh/git/practice/GitPracticeRun.java
@@ -5,7 +5,7 @@ public class GitPracticeRun {
 	public static void main(String[] args) {
 
 		System.out.println("안녕하세요~~");
-		System.out.println("Git 연습파일 입니다");
+		System.out.println("수정");
 
 	}
 
